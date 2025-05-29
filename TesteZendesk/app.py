028070481f390ac193005d6e5b110f394770b2f1
MuriloNogr <RM89162@fiap.com.br>
2025-05-29 19:04:43 -0300

    Application deployed with Fly.io

--- TesteZendesk/app.py
+++ TesteZendesk/app.py
@@ -62,5 +62,5 @@ def get_products():
     except Exception:
         return jsonify({"status": 500, "message": "Error parsing response"}), 500
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080, debug=True)
